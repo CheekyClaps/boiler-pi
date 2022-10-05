5949482dde9f58e479aa66d2e41bd546b9c28fb6
user <vincentvantrigt@protonmail.com>
2022-10-05 10:37:39 +0200

    init api, chartjs able to read api endpoints

--- website/api.py
+++ website/api.py
@@ -1,12 +1,68 @@
-from flask import Blueprint, jsonify
-from random import sample
+from flask import Blueprint, jsonify, Response, stream_with_context
+import json
+import random
+import time
+from datetime import datetime
+
 
 bp = Blueprint("api", __name__)
+random.seed()
+
+@bp.route("/target-temperature")
+def target():
+    def target_temp():
+        while True:
+            json_data = json.dumps(
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': 60})
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+    response = Response(stream_with_context(target_temp()), mimetype="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
+
 
 @bp.route("/temperature")
 def temperature():
-    return jsonify({'temperature' : sample(range(1,10), 5)}) 
+    def generate_random_data():
+        while True:
+            json_data = json.dumps(
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': random.random() * 50})
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+    response = Response(stream_with_context(generate_random_data()), mimetype="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
+
+
 
 @bp.route("/pressure")
 def pressure():
-    return jsonify({'pressure' : sample(range(1,10), 5)}) 
+    def generate_random_data():
+        while True:
+            json_data = json.dumps(
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': 40})
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+    response = Response(stream_with_context(generate_random_data()), mimetype="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
+
+@bp.route("/element-status")
+def element():
+    def generate_random_data():
+        while True:
+            json_data = json.dumps(
+                {'time': datetime.now().strftime('%H:%M:%S'), 'value': 10})
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+    response = Response(stream_with_context(generate_random_data()), mimetype="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ bp = Blueprint("views", __name__)
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+@bp.route("/chart")
+def chart():
+    return render_template("chart.html")
