c2f8ea1cf8190042b874e958dfdd1a469d05dabb
user <vincentvantrigt@protonmail.com>
2022-10-31 19:36:33 +0100

    set host to refelct prd

--- website/views.py
+++ website/views.py
@@ -6,14 +6,6 @@ bp = Blueprint("views", __name__)
 
 @bp.route("/", methods=['GET', 'POST'])
 def index():
-    safety = Safety_caveats.query.get(1)
-
-    print(safety)
-
-    flash(Target_temp.query.get(1), category='danger')
-
-    flash(Safety_caveats.mintemp, category='danger')
-
     new_target_temperature = request.form.get("target_temperature")
     if new_target_temperature and new_target_temperature >= '90':
         flash(new_target_temperature + " is too high!", category='danger')
@@ -22,7 +14,7 @@ def index():
     elif new_target_temperature:
         print(new_target_temperature)
 
-    return render_template("index.html")
+    return render_template("index.html", server_ip=request.environ)
 
 @bp.route("/chart")
 def chart():
