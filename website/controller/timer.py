1b7b091ad624195e0e7ce874b41002f26b097656
user <vincentvantrigt@protonmail.com>
2022-11-25 18:40:30 +0100

    implement timer, and reachable by index

--- website/controller/timer.py
+++ website/controller/timer.py
@@ -3,16 +3,26 @@ import time
 
 ssr1 = 14
 ssr2 = 15
+led = 8
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(ssr1, GPIO.OUT)
-GPIO.setup(ssr2, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ssr1, GPIO.OUT, initial=GPIO.LOW))
+GPIO.setup(ssr2, GPIO.OUT, initial=GPIO.LOW))
+GPIO.setup(led, GPIO.OUT, initial=GPIO.LOW))
 
-GPIO.output(ssr1, GPIO.HIGH)
-GPIO.output(ssr2, GPIO.HIGH)
-time.sleep(3600)
-GPIO.output(ssr1, GPIO.LOW)
-GPIO.output(ssr2, GPIO.LOW)
+try:
+    print("set GIOP high")
+    GPIO.output(ssr1, GPIO.HIGH)
+    GPIO.output(ssr2, GPIO.HIGH)
+    GPIO.output(led, GPIO.HIGH)
+    print("sleeping")
+    time.sleep(3600)
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    print("Keyboard interrupt")
 
-GPIO.cleanup()
+except:
+    print("some error")
 
+finally:
+    print("clean up")
+    GPIO.cleanup() # cleanup all GPIO
